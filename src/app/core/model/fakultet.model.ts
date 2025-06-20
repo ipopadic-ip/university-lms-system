038ba66877e6c7e0076890a828d3ef1d3ea54460
Ilija <83288042+ipopadic-ip@users.noreply.github.com>
2025-06-21 01:38:34 +0200

    Finalan Projekat

--- src/app/core/model/fakultet.model.ts
+++ src/app/core/model/fakultet.model.ts
@@ -1,21 +1,4 @@
-// export interface Dekan {
-//     ime: string;
-//     titula: string;
-//     opis: string;
-//     slika: string;
-//   }
-  
-//   export interface Fakultet {
-//     id: number;
-//     naziv: string;
-//     lokacija: string;
-//     kontakt: string;
-//     telefon: string;
-//     opis: string;
-//     slika: string;
-//     dekan: Dekan;
-//   }
-  
+
 export interface Fakultet {
   id: number;
   naziv: string;
